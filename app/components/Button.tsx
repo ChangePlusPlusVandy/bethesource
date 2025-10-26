4c60c639ccd346e12700f3f9fdd88bdcd57c3131
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-10-26 14:40:09 -0500

    skeleton code: Buttons

---
+++ app/components/Button.tsx
@@ -0,0 +1,16 @@
+"use client";
+import styled from "styled-components";
+
+// TODO: Add props for Button
+type ButtonProps = {
+  propName: string; // replace string with actual prop type
+};
+
+// TODO: implement Button component
+export default function Button({}: ButtonProps) {
+  return <StyledButton />;
+}
+
+const StyledButton = styled.p`
+  /* TODO: Add styles for Button */
+`;
