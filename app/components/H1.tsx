e3a93ab9c18ddd5b428d87655d86848dfd20f6e5
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-10-26 15:44:48 -0500

    update to use H1 component instead of Title, keep everything in the same place

--- app/components/H1.tsx
+++ app/components/H1.tsx
@@ -1,13 +1,30 @@
 "use client";
 import styled from "styled-components";
 
-// TODO: Add props for H1
+// TODO: fix compatability with color/design scheme
+type TextColorType =
+  | "white"
+  | "black"
+  | "gray"
+  | "red"
+  | "green"
+  | "blue"
+  | "yellow"
+  | "purple"
+  | "orange"
+  | "pink"
+  | "brown"
+  | "gray"
+  | "black"
+  | "white";
+
 type H1Props = {
-  propName: string; // replace string with actual prop type
+  text: string;
+  textcolor: TextColorType;
 };
 
 // TODO: implement H1 component
-export default function H1({}: H1Props) {
+export default function H1({ text, textcolor }: H1Props) {
   return <StyledH1 />;
 }
 
