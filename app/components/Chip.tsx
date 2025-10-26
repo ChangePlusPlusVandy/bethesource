af2c279fdcc9a582401361a8ed646df040e2d98a
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-10-26 14:45:09 -0500

    skeleton code: Chip

---
+++ app/components/Chip.tsx
@@ -0,0 +1,16 @@
+"use client";
+import styled from "styled-components";
+
+// TODO: Add props for Chip
+type ChipProps = {
+  propName: string; // replace string with actual prop type
+};
+
+// TODO: implement Chip component
+export default function Chip({}: ChipProps) {
+  return <StyledChip />;
+}
+
+const StyledChip = styled.div`
+  /* TODO: Add styles for Chip */
+`;
