f94f912a1436abd9e51abb21867c67ea15d8cf9d
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-10-26 14:09:42 -0500

    skeleton code: Headers

---
+++ app/components/H1.tsx
@@ -0,0 +1,16 @@
+"use client";
+import styled from "styled-components";
+
+// TODO: Add props for H1
+type H1Props = {
+  propName: string; // replace string with actual prop type
+};
+
+// TODO: implement H1 component
+export default function H1({}: H1Props) {
+  return <StyledH1 />;
+}
+
+const StyledH1 = styled.h1`
+  /* TODO: Add styles for H1 */
+`;
