f67aee8ba168753a73e699b56715733fe36da6c9
Rachel Koh <rachelkohhuiqi@gmail.com>
2025-10-26 14:16:46 -0500

    skeleton code: Text components

---
+++ app/components/TextRegular.tsx
@@ -0,0 +1,16 @@
+"use client";
+import styled from "styled-components";
+
+// TODO: Add props for TextRegular
+type TextRegularProps = {
+  propName: string; // replace with actual prop
+};
+
+// TODO: implement TextRegular component
+export default function TextRegular({}: TextRegularProps) {
+  return <StyledTextRegular />;
+}
+
+const StyledTextRegular = styled.p`
+  /* TODO: Add styles for TextRegular */
+`;
